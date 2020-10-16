fda9253c750fadf44defec3873ffe99a58e0541f
Ibtid <ibtid011@example.com>
2020-10-17 01:46:36 +0600

    icon styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
 import DonutLargeIcon from "@material-ui/icons/DonutLarge"
 import ChatIcon from "@material-ui/icons/Chat"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
@@ -10,10 +10,10 @@ const Sidebar = () => {
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar/>
-                <div className="sidebar__headeerRight">
-                    <DonutLargeIcon/>
-                    <ChatIcon/>
-                    <MoreVertIcon/>
+                <div className="sidebar__headerRight">
+                    <IconButton><DonutLargeIcon/></IconButton>
+                    <IconButton><ChatIcon/></IconButton>
+                    <IconButton><MoreVertIcon/></IconButton>
                 </div>
             </div>
             <div className="sidebar__search">
