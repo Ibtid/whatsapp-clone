c43a0f18d222c13bb7b6ad11ce25239f3fa3605c
Ibtid <ibtid011@example.com>
2020-10-16 21:59:32 +0600

    Sidebar shown

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./Sidebar.css"
 
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <h1>Sidebar</h1>
         </div>
     )
 }
