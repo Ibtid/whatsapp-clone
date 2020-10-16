ecf8fde0d9158947302943181b08ab229fb43b89
Ibtid <ibtid011@example.com>
2020-10-16 21:14:22 +0600

    Clean up and firebase config

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAxLezw7D5B67otF-R1QjclsBTUpzE58Ys",
+    authDomain: "whats-app-clone-526f4.firebaseapp.com",
+    databaseURL: "https://whats-app-clone-526f4.firebaseio.com",
+    projectId: "whats-app-clone-526f4",
+    storageBucket: "whats-app-clone-526f4.appspot.com",
+    messagingSenderId: "840716225567",
+    appId: "1:840716225567:web:ce1d33f226fac182ab592a",
+    measurementId: "G-CC3FJ3SLM8"
+  };
