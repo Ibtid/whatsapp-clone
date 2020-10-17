a8a44810894c29500bcd7563b680e762591ebbf6
Ibtid <ibtid011@example.com>
2020-10-17 20:09:20 +0600

    random ava

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,11 +1,18 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./SidebarChat.css"
 
 const SidebarChat = () => {
+
+    const [seed,setSeed] = useState(''); 
+    
+    useEffect(()=>{
+        setSeed(Math.floor(Math.random()*5000))
+    },[]);
+
     return (
         <div className="sidebarChat">
-            <Avatar src="https://avatars.dicebear.com/api/human/ibtid.svg"/>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className="sidebarChat___info">
                 <h2>Room name</h2>
                 <p>Last message...</p>
