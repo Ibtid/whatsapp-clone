4726aa5a9ca8febf89a7c11fb7def3d608dd49c7
Ibtid <ibtid011@example.com>
2020-10-19 00:12:44 +0600

    Chat message and receiver starter

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,5 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
+import {MoreVert,AttachFile, SearchOutlined} from "@material-ui/icons/"
 import React,{useState,useEffect} from 'react'
 import "./Chat.css"
 
@@ -6,15 +7,30 @@ const Chat = () => {
 
     const[seed,setSeed] = useState();
 
-    useEffect(() => {}, [])
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()*5000));
+    }, [])
 
     return (
         <div className="chat">
             <div className="chat__header">
-                <Avatar/>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton><SearchOutlined/></IconButton>
+                    <IconButton><AttachFile/></IconButton>
+                    <IconButton><MoreVert/></IconButton>
+                </div>
             </div>
             <div className="chat__body">
-
+                <p className={`chat__message ${true && "chat__receiver"}`}>
+                   <span className="chat__name">Ibtid</span>
+                   Hey Guys
+                   <span className="chat__timestamp">3:52pm</span>
+                </p>
             </div>
             <div className="chat__footer">
 
