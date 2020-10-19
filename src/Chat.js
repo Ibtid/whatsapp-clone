2c5180acc94e401af5704c34030925a1fc085b55
Ibtid <ibtid011@example.com>
2020-10-20 01:46:19 +0600

    bring the room name from db

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,7 @@ import { Avatar, IconButton } from '@material-ui/core'
 import {MoreVert,AttachFile, SearchOutlined, InsertEmoticon, MicOutlined} from "@material-ui/icons/"
 import React,{useState,useEffect} from 'react'
 import { useParams } from "react-router-dom"
+import db from "./firebase"
 import "./Chat.css"
 
 const Chat = () => {
@@ -9,6 +10,17 @@ const Chat = () => {
     const[seed,setSeed] = useState("");
     const [input,setInput] = useState("")
     const { roomId } = useParams();
+    const [roomName, setRoomName] = useState("");
+
+    useEffect(()=>{
+        if(roomId){
+            db.collection('rooms').doc(roomId).onSnapshot(
+                snapshot=>(
+                    setRoomName(snapshot.data().name)
+                )
+            )
+        }
+    },[roomId])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000));
@@ -26,7 +38,7 @@ const Chat = () => {
             <div className="chat__header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="chat__headerInfo">
-                    <h3>Room name</h3>
+                    <h3>{roomName}</h3>
                     <p>Last seen at ...</p>
                 </div>
                 <div className="chat__headerRight">
