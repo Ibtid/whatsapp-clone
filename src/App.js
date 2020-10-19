744406ebda60f3f4b57031d2bb97c0b38c5f1fb1
Ibtid <ibtid011@example.com>
2020-10-20 01:21:22 +0600

    router applied

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
       <div className="app__body">
-        <Sidebar/>
-        <Chat/>
+        <Router>
+          <Sidebar/>
+          <Switch>
+            <Route path ="/rooms/:roomId" exact>
+              <Chat/>
+            </Route>
+            <Route to = "/" exact>
+              <h1>asa</h1>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
