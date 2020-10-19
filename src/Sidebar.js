938e005e8919a7a77c25bdc75c5d61f11fa87126
Ibtid <ibtid011@example.com>
2020-10-19 20:08:18 +0600

    retrieved data from data base

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,12 +2,26 @@ import { Avatar, IconButton } from '@material-ui/core'
 import DonutLargeIcon from "@material-ui/icons/DonutLarge"
 import ChatIcon from "@material-ui/icons/Chat"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import "./Sidebar.css"
 import { SearchOutlined } from '@material-ui/icons'
 import SidebarChat from './SidebarChat'
+import db from "./firebase";
 
 const Sidebar = () => {
+    const [rooms,setRooms] = useState([]);
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshots=>(
+            setRooms(snapshots.docs.map(doc =>(
+                ({
+                    id:doc.id,
+                    data:doc.data(),
+                })
+            )))
+        ))
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -26,9 +40,13 @@ const Sidebar = () => {
             </div>
             <div className="sidebar__chats">
                 <SidebarChat addNewChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
+                {rooms.map(room => (
+                    <SidebarChat
+                       key={room.id}
+                       id={room.id}
+                       name={room.data.name}
+                    />
+                ))}
             </div>
         </div>
     )
