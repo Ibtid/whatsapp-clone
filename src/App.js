f596fc89dc70ab84853b6ad639149666ac25b3b2
Ibtid <ibtid011@example.com>
2020-10-21 01:23:49 +0600

    login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from './Login';
 
 function App() {
+  const[user,setUser]=useState(null);
+  
   return (
     //BEM naming convention
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar/>
-          <Switch>
-            <Route path ="/rooms/:roomId" exact>
-              <Chat/>
-            </Route>
-            <Route to = "/" exact>
-              <h1>asa</h1>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+         <Login/>
+        ):(
+          <div className="app__body">
+            <Router>
+              <Sidebar/>
+              <Switch>
+                <Route path ="/rooms/:roomId" exact>
+                  <Chat/>
+                </Route>
+                <Route to = "/" exact>
+                  <h1>asa</h1>
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        )
+      }
     </div>
   );
 }
