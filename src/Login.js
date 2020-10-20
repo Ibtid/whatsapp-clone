ef7ec64455dbec3d47f9728ee2d32299717cf139
Ibtid <ibtid011@example.com>
2020-10-21 01:35:44 +0600

    google authentication

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,17 @@
 import { Button } from '@material-ui/core'
+import { auth, provider } from './firebase';
 import React from 'react'
 import "./Login.css"
 
 const Login = () => {
 
-    const signIn = () => {};
+    const signIn = () => {
+        auth.signInWithPopup(provider).then(
+            (result) => console.log(result)
+        ).catch(
+            (error) => alert(error.message)
+        );
+    };
 
     return (
         <div className="login">
